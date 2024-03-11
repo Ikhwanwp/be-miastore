ece8d037f254b6fe0f278558b7e64a7832a57fbb
Ikhwanwp <wiratama900@gmail.com>
2024-03-11 18:40:00 +0700

    DashboardAdmin

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html class="no-js" lang=""> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MiaStoreAdmin</title>
+    <meta name="description" content="ShaynaAdmin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- Style --}}
+    @include('includes.style')
+</head>
+
+<body>
+    <!-- Left Panel -->
+    @include('includes.sidebar')
+    <!-- /#left-panel -->
+    <!-- Right Panel -->
+    <div id="right-panel" class="right-panel">
+        <!-- Header-->
+        @include('includes.navbar')
+        <!-- /#header -->
+        <!-- Content -->
+        <div class="content">
+            <!-- Animated -->
+            @yield('content')
+            <!-- .animated -->
+        </div>
+        <!-- /.content -->
+        <div class="clearfix"></div>
+    </div>
+    <!-- /#right-panel -->
+
+    <!-- Scripts -->
+    @include('includes.script')
+</body>
+</html>
