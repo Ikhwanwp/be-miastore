90c7b773150c77da7441db51b811e61575b984cc
Ikhwanwp <wiratama900@gmail.com>
2024-03-12 12:53:13 +0700

    Add List Create

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
+// Agar tidak bisa melakukan registrasi lagi
 Auth::routes(['register' => false]);
+
+Route::resource('products', ProductController::class);
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
